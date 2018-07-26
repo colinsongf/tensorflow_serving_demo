dece933b121b66c973bb34b24b5220cd8ca2da57
zhangpf2 <zhangpf2@mapbar.com>
2018-07-26 17:46:50 +0800

    renew

--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 from grpc.beta import implementations
 import numpy as np
 from PIL import Image
-import os
-from flask import jsonify, render_template, make_response, send_from_directory
 
 import tensorflow as tf
 from tensorflow_serving.apis import predict_pb2
@@ -11,11 +9,6 @@ from tensorflow_serving.apis import prediction_service_pb2
 server = 'localhost:8500'
 model_name = 'deeplab'
 signature_name = 'predict_image'
-src_dir = './data/sources'
-tmp_dir = './data/results'
-dst_dir = './data/labels'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF'])
-
 
 host, port = server.split(':')
 _channel = implementations.insecure_channel(host, int(port))
@@ -26,7 +19,7 @@ _TFrequest.model_spec.name = model_name
 _TFrequest.model_spec.signature_name = signature_name
 
 
-def tf_predict(image_name, request=_TFrequest, stub=_TFstub):
+def tf_predict(image_path, mask_path, request=_TFrequest, stub=_TFstub):
     """Predict image segmentation using TF Server
     Args:
         image_name: str
@@ -34,7 +27,6 @@ def tf_predict(image_name, request=_TFrequest, stub=_TFstub):
         res: dict {'status': int, 'mask_path': str, 'height': int, 'width': int}.
             status: 0 is SUCCESS, 1 is FileNotFound, 2 is TFServerError, 3 is MaskSaveError, 4 is OtherErroe
     """
-    image_path = os.path.join(src_dir, image_name)
     res = {'status': 104, 'mask_path': 'null', 'height': 0, 'width': 0}
 
     try:
@@ -59,62 +51,12 @@ def tf_predict(image_name, request=_TFrequest, stub=_TFstub):
     try:
         output = np.reshape(output, (height, width)).astype(np.uint8)
         mask = Image.fromarray(output)
-        mask_path = image_path.replace('sources', 'results').replace('jpg', 'png')
         mask.save(mask_path)
     except:
         res['status'] = 103
         return res
     else:
         res['status'] = 0
-        res['mask_path'] = os.path.basename(mask_path)
+        res['mask_path'] = mask_path.split('/')[-1]
         return res
 
-
-def get_list():
-    """Get image list. This is irrelevant to TF serving, just an extra task for me."""
-    lines = []
-    for root, dirs, files in os.walk(src_dir):
-        level = root.replace(src_dir, '').count(os.sep)
-        indent = '-' * 2 * level
-        lines.append('{}{}/'.format(indent, os.path.basename(root)) + '\n')
-        # fileSave.write('{}{}/'.format(indent, os.path.abspath(root)) + '\n')
-        sub_indent = '-' * 2 * (level + 1)
-        for f in files:
-            # fileSave.write('{}{}'.format(subIndent, f) + '\n')
-            lines.append('{}{}'.format(sub_indent, f) + '\n')
-    return {'files': lines}
-
-
-def check_format(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-def uploads(file):
-    if not check_format(file.filename):
-        return jsonify({'status': 101})
-
-    path = os.path.join(dst_dir, file.filename)
-    if os.path.exists(path):
-        return jsonify({'status': 102})
-
-    try:
-        file.save(path)
-    except:
-        return jsonify({'status': 103})
-    else:
-        return jsonify({'status': 0})
-
-
-def downloads(filename):
-    split, name = filename.strip().split('_')
-    if split == 'src':
-        path = src_dir
-    elif split == 'res':
-        path = tmp_dir
-    else:
-        return jsonify({'status': 101})
-
-    if not os.path.exists(os.path.join(path, filename)):
-        return jsonify({'status': 102})
-
-    return send_from_directory(path, filename, as_attachment=True)
--- view.py
+++ view.py
@@ -1,40 +1,88 @@
-from flask import Flask, jsonify, request, render_template
-from utils import tf_predict, get_list, uploads, downloads
+import os
+from flask import Flask, jsonify, request, render_template, send_from_directory
+from utils import tf_predict
 
 app = Flask(__name__)
+SRC_DIR = './data/sources'
+TMP_DIR = './data/results'
+DST_DIR = './data/labels'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF'])
 
 
-@app.route("/", methods=["POST", "GET"])
-def tfpredict():
+def check_format(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route("/")
+def test():
+    return render_template("index.html")
+
+
+@app.route("/api/predict", methods=["POST", "GET"])
+def predict():
     if request.method == "POST":
         image_name = request.form.get("image_name", "null")
-        res = tf_predict(str(image_name))
+        image_path = os.path.join(SRC_DIR, image_name)
+        mask_path = image_path.replace('sources', 'results').replace('jpg', 'png')
+        res = tf_predict(image_path, mask_path)
         return jsonify(res)
     else:
         return render_template("index.html")
 
 
-@app.route('/list', methods=["POST", "GET"])
+@app.route('/api/getlist', methods=["POST", "GET"])
 def getlist():
     """Note: this interface is irrelevant to TF serving, just an extra task for me.
     """
     if request.method == "POST":
-        res = get_list()
+        res = {'status': 0, 'files': []}
+        lines = []
+        for root, dirs, files in os.walk(SRC_DIR):
+            level = root.replace(SRC_DIR, '').count(os.sep)
+            indent = '-' * 2 * level
+            lines.append('{}{}/'.format(indent, os.path.basename(root)) + '\n')
+            # fileSave.write('{}{}/'.format(indent, os.path.abspath(root)) + '\n')
+            sub_indent = '-' * 2 * (level + 1)
+            for f in files:
+                # fileSave.write('{}{}'.format(subIndent, f) + '\n')
+                lines.append('{}{}'.format(sub_indent, f) + '\n')
+        res['files'] = lines
         return jsonify(res)
     else:
         return render_template("index.html")
 
 
-@app.route('/upload', methods=["POST"])
+@app.route('/api/upload', methods=["POST"])
 def upload():
-    f = request.files.get('image_name')
-    return uploads(f)
+    file = request.files.get('image_name')
+    if not check_format(file.filename):
+        return jsonify({'status': 101})
+
+    path = os.path.join(DST_DIR, file.filename)
+    if os.path.exists(path):
+        return jsonify({'status': 102})
+
+    try:
+        file.save(path)
+    except:
+        return jsonify({'status': 103})
+    else:
+        return jsonify({'status': 0})
 
 
-@app.route('/download/<path:filename>', methods=["POST", "GET"])
+@app.route('/api/download/<path:filename>', methods=["POST", "GET"])
 def download(filename):
     if request.method == "GET":
-        return downloads(filename)
+        path = ''
+        split, name = filename.strip().split('_')
+        if split == 'src':
+            path = SRC_DIR
+        elif split == 'res':
+            path = TMP_DIR
+        print(path)
+        return send_from_directory(path, name, as_attachment=True)
+    else:
+        return render_template("index.html")
 
 
 @app.before_first_request
@@ -46,5 +94,5 @@ if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
         port=8080,
-        debug=False
+        debug=True
     ) 
